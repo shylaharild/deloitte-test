d7dcc07a83eecca03edd41494ac10699cd9176d2
Shyla Hari <shyla.hari@polestarglobal.com>
2018-08-29 18:23:03 +0100

    SL-DEVOPS Lambda handler.py file to generate random number between 1 & 100

---
+++ random_number/handler.py
@@ -0,0 +1,15 @@
+import json
+import random
+
+def website(event, context):
+    random_number = random.randint(1,101)
+    body = {
+        "message": "The random number is " + str(random_number)
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
